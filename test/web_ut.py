7b2bcc610127abbf4de4e617d291a8fa5e90a5ff
maye102 <49386749+maye102@users.noreply.github.com>
2022-01-21 22:54:40 +0800

    Update web_ut.py

--- test/web_ut.py
+++ test/web_ut.py
@@ -28,6 +28,7 @@ class ISelenium(unittest.TestCase):
         if using_headless is not None and using_headless.lower() == 'true':
             print('使用无界面方式运行')
             chrome_options.add_argument("--headless")
+        print("============",chrome_options)
         self.driver = webdriver.Chrome(executable_path="D:\chromedirver\chromedriver.exe",options=chrome_options)
 
 
