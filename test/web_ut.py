0e4fdb6f923e9b9f850f491b20fb0fa6200fa939
zhang <zhang@163.com>
2022-01-21 21:35:18 +0800

    优化

--- test/web_ut.py
+++ test/web_ut.py
@@ -5,7 +5,6 @@ import time
 import unittest
 
 from selenium import webdriver
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 
@@ -15,7 +14,7 @@ class ISelenium(unittest.TestCase):
     # 读入配置文件
     def get_config(self):
         config = configparser.ConfigParser()
-        config.read(os.path.join(os.environ['HOME'], 'iselenium.ini'))
+        config.read(os.path.join(os.path.dirname(os.path.abspath(".")),'iselenium.ini'))
         return config
 
     def tearDown(self):
@@ -26,26 +25,19 @@ class ISelenium(unittest.TestCase):
 
         # 控制是否采用无界面形式运行自动化测试
         try:
-            browser = os.environ["browser"]
+            using_headless = os.environ["using_headless"]
         except KeyError:
-            browser = None
-            print('没有配置环境变量 browser, 按照默认有界面方式运行自动化测试')
+            using_headless = None
+            print('没有配置环境变量 using_headless, 按照默认有界面方式运行自动化测试')
 
         chrome_options = Options()
-        if browser is not None and browser.lower() == 'no_gui':
+        if using_headless is not None and using_headless.lower() == 'true':
             print('使用无界面方式运行')
             chrome_options.add_argument("--headless")
-            self.driver = webdriver.Chrome(executable_path=config.get('driver', 'chrome_driver'),
+        print(config.get('driver', 'chrome_driver'))
+        self.driver = webdriver.Chrome(executable_path=config.get('driver', 'chrome_driver'),
                                            options=chrome_options)
-        elif browser is not None and browser.lower() == 'remote':
-            docker_remote = config.get('driver', 'remote')
-            print(f'使用远程方式运行, remote url:{docker_remote}')
-            self.driver = webdriver.Remote(command_executor=docker_remote,
-                                           desired_capabilities=DesiredCapabilities.CHROME)
-        else:
-            print('使用有界面Chrome浏览器运行')
-            self.driver = webdriver.Chrome(executable_path=config.get('driver', 'chrome_driver'),
-                                           options=chrome_options)
+
 
     @allure.story('Test key word 今日头条')
     def test_webui_1(self):
@@ -70,14 +62,14 @@ class ISelenium(unittest.TestCase):
 
         self.driver.get("https://www.baidu.com")
         print('打开浏览器，访问 www.baidu.com')
-        time.sleep(5)
+        time.sleep(3)
         assert f'百度一下' in self.driver.title
 
         elem = self.driver.find_element_by_name("wd")
         elem.send_keys(f'{search_keyword}{Keys.RETURN}')
         print(f'搜索关键词~{search_keyword}')
 
-        time.sleep(5)
+        time.sleep(3)
         result = f'{search_keyword}' in self.driver.title
         self.assertTrue(result, msg=f'{testcase_name}校验点 pass')
 
